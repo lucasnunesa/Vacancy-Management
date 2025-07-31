944a691936d7ce3ea5946aeaa533211c568a87fd
Lucas Nunes <lucas.na@sempreceub.com>
2025-07-30 21:35:49 -0300

    feat: add jobDTO for response.

---
+++ src/main/java/br/com/nunes/vacancy/management/dto/JobDTO.java
@@ -0,0 +1,11 @@
+package br.com.nunes.vacancy.management.dto;
+
+import lombok.Data;
+
+@Data
+public class JobDTO {
+
+    private String description;
+    private String title;
+    private String level;
+}
