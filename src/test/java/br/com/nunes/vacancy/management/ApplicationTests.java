fd407371f253e204d3190ed86e567998df116a99
Lucas Nunes <lucas.na@sempreceub.com>
2025-07-30 21:32:53 -0300

    build: first commit with project build.

---
+++ src/test/java/br/com/nunes/vacancy/management/ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.nunes.vacancy.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
