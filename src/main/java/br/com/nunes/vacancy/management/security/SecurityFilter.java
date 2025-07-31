16528f0f71346f7143ec8a2a9a38f5d2e6419b39
Lucas Nunes <lucas.na@sempreceub.com>
2025-07-30 21:34:47 -0300

    feat: security module for authorization.

---
+++ src/main/java/br/com/nunes/vacancy/management/security/SecurityFilter.java
@@ -0,0 +1,46 @@
+package br.com.nunes.vacancy.management.security;
+
+import br.com.nunes.vacancy.management.providers.JWTProvider;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private JWTProvider jwtProvider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+        SecurityContextHolder.getContext().setAuthentication(null);
+
+        String token = request.getHeader("Authorization");
+
+        if (token != null) {
+            String subjectToken = this.jwtProvider.validateToken(token);
+
+            if (subjectToken.isEmpty()) {
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                response.getWriter().write("Invalid token");
+                return;
+            }
+            request.setAttribute("company_id", subjectToken);
+            UsernamePasswordAuthenticationToken authentication =
+                    new UsernamePasswordAuthenticationToken(subjectToken, null, null);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
