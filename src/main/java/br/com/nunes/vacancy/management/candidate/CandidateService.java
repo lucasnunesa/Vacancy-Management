7c264ed2a06d3aec1770d9a4b3f2cb1201fb0232
Lucas Nunes <lucas.na@sempreceub.com>
2025-08-03 20:40:28 -0300

    fix: fixed a problem where the password was being persisted after the encode, breaking a business rule of size/length.

--- src/main/java/br/com/nunes/vacancy/management/candidate/CandidateService.java
+++ src/main/java/br/com/nunes/vacancy/management/candidate/CandidateService.java
@@ -2,16 +2,24 @@ package br.com.nunes.vacancy.management.candidate;
 
 import br.com.nunes.vacancy.management.exceptions.UserAlreadyExistException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CandidateService {
 
     @Autowired
-    protected CandidateRepository candidateRepository;
+    private CandidateRepository candidateRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     public Candidate addCandidate (Candidate candidate) {
         validateUsername(candidate.getUsername());
+        validatePassword(candidate.getPassword());
+
+        String encodedPassword = passwordEncoder.encode(candidate.getPassword());
+        candidate.setPassword(encodedPassword);
 
         return candidateRepository.save(candidate);
     }
@@ -20,5 +28,11 @@ public class CandidateService {
         this.candidateRepository.findByUsername(username).ifPresent(candidate -> {
             throw new UserAlreadyExistException();
         });
+    }
+
+    private void validatePassword (String password) {
+        if (password.length() < 8 || password.length() > 16) {
+            throw new IllegalArgumentException("Password must be between 8 and 16 characters.");
+        }
     }
 }
