47c340f047805e884bb597c0e997487ceaa38822
Lucas Nunes <lucas.na@sempreceub.com>
2025-08-03 23:59:06 -0300

    update: update job controller for work with roles.

--- src/main/java/br/com/nunes/vacancy/management/jobs/JobController.java
+++ src/main/java/br/com/nunes/vacancy/management/jobs/JobController.java
@@ -4,6 +4,7 @@ import br.com.nunes.vacancy.management.dto.JobDTO;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,13 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/jobs")
+@RequestMapping("/company/job")
 public class JobController {
 
     @Autowired
     private JobService jobService;
 
     @PostMapping
+    @PreAuthorize("hasRole('COMPANY')")
     public ResponseEntity<Job> createJob (@RequestBody JobDTO jobDTO, HttpServletRequest request) {
         Object requestCompanyId = request.getAttribute("company_id");
 
@@ -32,8 +34,6 @@ public class JobController {
         Job createdJob = jobService.createJob(job);
 
         return ResponseEntity.ok(createdJob);
-
-
 
     }
 
